7c2e5512e27a379963769c580c6c2665c28b28fa
Matt Fenwick <mfenwick100@gmail.com>
2017-12-07 12:21:45 -0500

    use older syntax

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function parse(stage, text) {
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!')
+});
 
 
 app.get('/parse', function(req, res) {
