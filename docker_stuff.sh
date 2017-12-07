565fe2efd4ea3732d974b5d5c5d763766f1ed175
Matt Fenwick <mfenwick100@gmail.com>
2017-12-07 12:22:07 -0500

    add a couple Docker incantations

---
+++ docker_stuff.sh
@@ -0,0 +1,5 @@
+#   to create an image:
+# $ docker build -t mfenwickbd/clojarse-server .
+
+#   to run this image, hitting it with http://localhost:3011/parse?text=1 :
+# $ docker run -P -dp 3011:3000 mfenwickbd/clojarse-server
